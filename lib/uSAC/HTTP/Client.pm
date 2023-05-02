7e4d768d8fd649374a314edf10fcb8d4dfbe957b
Ruben Westerberg <drclaw@mac.com>
2023-05-02 14:11:23 +1000

    Moved client request code into client module

--- lib/uSAC/HTTP/Client.pm
+++ lib/uSAC/HTTP/Client.pm
@@ -1,7 +1,10 @@
 package uSAC::HTTP::Client;
+use feature "state";
 use Log::ger;
 use Object::Pad;
 
+use uSAC::HTTP::Route;
+use uSAC::HTTP::Session;
 class uSAC::HTTP::Client :isa(uSAC::HTTP::Server);
 
 
@@ -12,10 +15,17 @@ field $_host_pool_limit :param=undef;
 field $_cookie_jar_path :param=undef;
 field $_cv;
 
+field $_sessions;
+field $_read_size;
+
+field $_zombies;
+field $_zombie_limit;
+field $_application_parser;
 
 BUILD {
   $self->mode=1;          # Set mode to client.
   $_host_pool_limit//=5;  # limit to 5 concurrent connections by default
+  $_zombies=[];
 
 }
 
@@ -48,6 +58,210 @@ method run {
 	$_cv=AE::cv;
 	$_cv->recv();
 }
+
+
+my $session_id=0;
+# process the queue for a host table, given the session to reuse.
+# If no session is provided then create a new one, up to limit
+method _request {
+  Log::OK::TRACE and log_trace __PACKAGE__." _request";
+  die "_request must be made with a host table" unless @_ >=1;
+  # From the given host table and session attmempt to execute the request stored in $details
+  # or of extract from the host queue
+  # Schedules the request for the next loop of the event system
+  #
+  my ($table, $session)=@_;
+
+  #my($host, $port, $method, $uri, $header, $payload, $cb)=@_;
+
+
+  my $details=shift($table->[uSAC::HTTP::Site::REQ_QUEUE]->@*);
+
+
+  # If a session is supplied reuse it if possible
+  #
+  if($session){
+    $session->closeme=1;
+    $session->drop;
+    Log::OK::INFO and log_info "Session reuse attempted";
+      $table->[uSAC::HTTP::Site::ACTIVE_COUNT]--;
+
+      unless($details){
+        # No more work to do so add the session to the idle pool
+        #
+        Log::OK::INFO and log_info "No futher requests to process for this host table. Return to idle pool";
+        #push $table->[uSAC::HTTP::Site::IDLE_POOL]->@*, $session;
+        return;
+      }
+      $table->[uSAC::HTTP::Site::ACTIVE_COUNT]++;
+      return $self->__request($table, $session, $details) 
+  }
+
+  return unless $details;
+  # parse the host and port from the table
+  my $host=$details->[0];#Host;
+  my ($__host, $port)=split ":", $host; 
+  $port//=80;
+
+  my $limit =10;
+  if( 
+    ($limit<=0 or $table->[uSAC::HTTP::Site::ACTIVE_COUNT] < $limit)  # Check limit
+
+    ){
+
+      $table->[uSAC::HTTP::Site::ACTIVE_COUNT]++;
+
+      #Create another connection so long as it is doesn't exceed the limit
+      $self->do_stream_connect($__host, $port, sub {
+          my ($socket, $addr)=@_;
+          $table->[uSAC::HTTP::Site::ADDR]=$addr;
+          # Create a session here
+          #
+          my $scheme="http";
+          Log::OK::TRACE and log_trace __PACKAGE__." CRATEING NEW SESSION";
+
+          my $session=uSAC::HTTP::Session->new;
+          $session->init($session_id, $socket, $_sessions, $_zombies, $self, $scheme, $addr, $_read_size);
+
+          unless($_application_parser){
+            require uSAC::HTTP::v1_1_Reader;
+            $_application_parser=\&uSAC::HTTP::v1_1_Reader::make_parser;
+          }
+
+          say "APPLICATION PARSER: ".$_application_parser;
+
+          $session->push_reader($_application_parser->(session=>$session, mode=>1, callback=>sub {say "DUMMY PARSER CALLBACK====="}));
+        $_sessions->{ $session_id } = $session;
+
+        $session_id++;
+        $self->__request($table, $session, $details);
+
+
+
+
+
+
+
+      },
+
+      sub {
+        # connection error
+        #
+        $table->[uSAC::HTTP::Site::ACTIVE_COUNT]--;
+        say "error callback for stream connect: $_[1]";
+        IO::FD::close $_[0]; # Close the socket
+        my($route, $captures)=$table->[uSAC::HTTP::Site::HOST_TABLE_DISPATCH]($details->[1]." ".$details->[2]);
+
+        die "No route found for $host" unless $route;
+
+        $route->[1][ROUTE_ERROR_HEAD]->($route);
+        
+        #say $_[1];
+        #say Error::Show::context frames=>Devel::StackTrace->new();
+
+      }
+    )
+  }
+  else{
+    # Sit tight.. a connection will become available soon.. hopefully!
+    Log::OK::TRACE and log_trace __PACKAGE__. " request queued but waiting for in flight to finish";
+  }
+}
+
+
+method request {
+  Log::OK::TRACE and log_trace __PACKAGE__." request";
+  # Queue a request in host pool and trigger if queue is inactive
+  #
+  my($host, $method, $uri, $header, $payload, $cb)=@_;
+
+  $header//={};
+  $payload//="";
+  state $request_id=0;
+
+  my $options;
+
+  # Attempt to connect to host if we have no connections  in the pool
+  state $any_host=$self->host_tables->{"*.*"};
+
+  my $entry=$self->host_tables->{$host}//$any_host;
+  
+  # Push to queue
+
+
+  push $entry->[uSAC::HTTP::Site::REQ_QUEUE]->@*,
+    [$host, $method, $uri, $header, $payload, $cb, $request_id++, $entry];
+
+  # kick off queue processin if needed
+  #
+  $self->_request($entry) if $self->running_flag;
+  $request_id;
+}
+
+#  Push a request to the request queue
+#
+method __request {
+
+  my ($table, $session, $details)=@_;
+
+
+  my $version;
+  my $ex;
+  #my $id;
+  #my $fh;
+  my $scheme;
+  my $peers;
+  my $i;
+  
+  uSAC::IO::asap {
+
+    my $host=$details->[0];
+    my $method=$details->[1];
+    my $uri=$details->[2];
+    my $header=$details->[3];
+    my $payload=$details->[4];
+    my $cb=$details->[5];
+
+    say "PAYLOAD FOR REQUEST: $payload";
+    # At this point there should be at least one available session in the pool for the host
+
+    #say "Idle pool is: ".Dumper $table->[IDLE_POOL];
+    #my $session=pop $table->[uSAC::HTTP::Site::IDLE_POOL]->@*;
+    #$table->[uSAC::HTTP::Site::ACTIVE_COUNT]++;
+
+    # Do a route lookup
+    #
+    #say Dumper $details;
+    my($route, $captures)=$table->[uSAC::HTTP::Site::HOST_TABLE_DISPATCH]("$method $uri");
+
+    die "No route found for $host" unless $route;
+
+    #  Obtain session or create new. Update with the filehandle
+    my %h=();#%$_static_headers;   #Copy static headers
+
+
+
+    # Create the REX object
+    #
+    $ex=$session->exports;
+    $version="HTTP/1.1"; # TODO: FIX
+    my $rex=uSAC::HTTP::Rex::new("uSAC::HTTP::Rex", $session, \%h, $host, $version, $method, $uri, $ex, $captures);
+
+
+    # Set the current rex and route for the session.
+    # This is needed for the parser in client mode. It makes the route known
+    # ahead of time.
+    $ex->[3]->$*=$rex;
+    $ex->[7]->$*=$route;
+
+
+    # Call the head of the outerware function
+    #
+    $route->[1][ROUTE_OUTER_HEAD]($route, $rex, my $code=-1, $header, $payload, $cb);
+  };
+}
+
+
 
 
 # Like clicking the link
